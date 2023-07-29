fcb299cb984d02973c9fff7ce0ce306a9d173eac
bgoureige <bgoureige@clever-age.com>
2023-07-29 04:58:13 +0200

    first commit, upload and dowload fonction

---
+++ README.md
@@ -0,0 +1,1 @@
+# app_file_manager_backend
---
+++ app.js
@@ -0,0 +1,59 @@
+// index.js (ou app.js)
+
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors({ origin: 'http://localhost:4200' }));
+const PORT = 3000; // Vous pouvez modifier le port selon vos besoins
+
+// Configurer Multer pour l'upload des fichiers
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads'); // Dossier de destination pour les fichiers téléchargés
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname)); // Renommer le fichier téléchargé (utilisation d'un timestamp pour éviter les doublons)
+  },
+});
+const upload = multer({ storage });
+
+// Route pour l'upload de fichier
+app.post('/upload', upload.single('file'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'Aucun fichier téléchargé' });
+  }
+  res.json({ message: 'Fichier téléchargé avec succès', filename: req.file.filename });
+});
+
+// Route pour le download de fichier
+app.get('/download/:filename', (req, res) => {
+  const filename = req.params.filename;
+  const filePath = path.join(__dirname, 'uploads', filename);
+
+  res.download(filePath, (err) => {
+    if (err) {
+      res.status(404).json({ error: 'Fichier non trouvé' });
+    }
+  });
+});
+
+// Route pour la récupération de la liste des fichiers disponibles
+app.get('/api/files', (req, res) => {
+  const filesDirectory = path.join(__dirname, 'uploads');
+  console.log(filesDirectory)
+  fs.readdir(filesDirectory, (err, files) => {
+    if (err) {
+      return res.status(500).json({ error: 'Erreur lors de la récupération de la liste des fichiers' });
+    }
+    res.json(files);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
+});
